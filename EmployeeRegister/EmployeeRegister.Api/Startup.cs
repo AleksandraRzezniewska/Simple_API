224b324d58179af52ca61fb4ae88cb19feefb40a
Aleksandra Rzezniewska <o.rzezniewska@gmail.com>
2020-01-23 23:37:17 +0100

    add repository with generic methods

--- EmployeeRegister/EmployeeRegister.Api/Startup.cs
+++ EmployeeRegister/EmployeeRegister.Api/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeRegister.Common.Configuration;
+using EmployeeRegister.Common.Interfaces;
+using EmployeeRegister.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +29,8 @@ namespace EmployeeRegister.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.Configure<ApplicationConfiguration>(Configuration)
+                .AddSingleton<IDbContextFactory<EmployeeRegisterDbContext>, EmployeeRegisterDbContextFactory>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ EmployeeRegister/EmployeeRegister.Database/EmployeeRegisterDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeRegister.Common.Configuration;
+using EmployeeRegister.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace EmployeeRegister.Database
+{
+    public class EmployeeRegisterDbContextFactory : IDbContextFactory<EmployeeRegisterDbContext>
+    {
+        private readonly ApplicationConfiguration _applicationConfiguration;
+
+        public EmployeeRegisterDbContextFactory(IOptions<ApplicationConfiguration> options)
+        {
+            _applicationConfiguration = options.Value;
+        }
+
+        public EmployeeRegisterDbContext Create()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EmployeeRegisterDbContext>();
+
+            optionsBuilder.UseSqlServer(_applicationConfiguration.ConnectionStrings); ;
+
+            return new EmployeeRegisterDbContext(optionsBuilder.Options);
+        }
+    }
+}
